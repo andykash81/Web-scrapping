7a3cf8d66d0e92e4da4b38dc51bfa52140acb6fa
andykash81 <k81@ngs.ru>
2021-09-10 20:55:47 +0700

    Исправление ошибки AttributeError

--- main.py
+++ main.py
@@ -22,11 +22,14 @@ def search_text(text_for_analise, list__):
                     print(f"{date} - {title} - {URL + link_}")
                     list__.append(word)
                     return 'ok'
+        else:
+            compare = 'not ok'
 
 
 if __name__ == '__main__':
     response = requests.get('https://habr.com/ru/all/')
     response.raise_for_status()
+    response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, features='html.parser')
     articles = soup.find_all("article", class_="tm-articles-list__item")
     for article in articles:
@@ -39,8 +42,11 @@ if __name__ == '__main__':
         if query == 'ok':
             continue
         else:
-            title_ = article.find("h2").find('span').text
-            query = search_text(title_, list_)
+            try:
+                title_ = article.find("h2").find('span').text
+                query = search_text(title_, list_)
+            except AttributeError:
+                continue
         if query == 'ok':
             continue
         else:
